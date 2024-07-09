aee5a9550a70e0277fe3bd33b257b20dfa61d978
Rico <rico.flemming@student.htw-berlin.de>
2024-07-09 17:19:17 +0200

    create checkoutservice project

---
+++ checkoutservice/src/test/java/com/ribaso/checkoutservice/CheckoutserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ribaso.checkoutservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
