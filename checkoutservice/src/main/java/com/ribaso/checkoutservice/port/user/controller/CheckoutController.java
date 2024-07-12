f88d6e60843ac8f13a26d341c47a30c9d59a7496
Rico <rico.flemming@student.htw-berlin.de>
2024-07-12 18:15:49 +0200

    SwaggerDoku

--- checkoutservice/src/main/java/com/ribaso/checkoutservice/port/user/controller/CheckoutController.java
+++ checkoutservice/src/main/java/com/ribaso/checkoutservice/port/user/controller/CheckoutController.java
@@ -10,6 +10,12 @@ import com.ribaso.checkoutservice.core.domain.service.interfaces.CheckoutService
 import com.ribaso.checkoutservice.port.exception.CheckoutAlreadyCompletedException;
 import com.ribaso.checkoutservice.port.exception.InvalidCheckoutIdException;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
@@ -18,12 +24,20 @@ public class CheckoutController {
     private CheckoutService checkoutService;
 
     @PostMapping("/initiate")
+    @Operation(summary = "Initiate checkout", description = "Initiates a new checkout process and returns the checkout details.")
     public Checkout initiateCheckout() {
         return checkoutService.initiateCheckout();
     }
 
     @PostMapping("/complete/{id}")
-    public ResponseEntity<?> completeCheckout(@PathVariable Long id) {
+    @Operation(summary = "Complete checkout", description = "Completes a checkout process for a given ID.")
+    @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Checkout completed successfully", content = @Content(schema = @Schema(implementation = Checkout.class))),
+    @ApiResponse(responseCode = "404", description = "Invalid Checkout ID: Provided ID does not exist", content = @Content(schema = @Schema(implementation = String.class))),
+    @ApiResponse(responseCode = "400", description = "Checkout is already completed for ID: Provided ID has already been completed", content = @Content(schema = @Schema(implementation = String.class))),
+    @ApiResponse(responseCode = "500", description = "An unexpected error occurred", content = @Content(schema = @Schema(implementation = String.class)))
+})
+    public ResponseEntity<?> completeCheckout(@PathVariable  @Parameter(description = "The unique identifier of the checkout to complete.") Long id) {
         try {
             Checkout checkout = checkoutService.completeCheckout(id);
             return ResponseEntity.ok(checkout);
